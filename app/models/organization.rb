519039fc0ec528c4f70de36d3f39823fc8d3a266
Henrique Albuquerque <henrique.alb@me.com>
2021-09-06 17:38:23 +0100

    validations for organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,8 @@
 class Organization < ApplicationRecord
+  has_many :users, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :venues, through: :events
+  has_one_attached :logo
+
+  validates :name, presence: true, uniqueness: true
 end
