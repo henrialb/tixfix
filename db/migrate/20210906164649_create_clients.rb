292835eb33ab0d46513d9ab702a9f45f49e12899
Pedro Rato <rato.pedromoreira@gmail.com>
2021-09-06 17:48:37 +0100

    Created Clients model and migration

---
+++ db/migrate/20210906164649_create_clients.rb
@@ -0,0 +1,11 @@
+class CreateClients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
