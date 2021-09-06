b778b2871aeb1052de46c319d90c54f020417d7a
Pedro Rato <rato.pedromoreira@gmail.com>
2021-09-06 17:53:24 +0100

    Created Orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_164649) do
+ActiveRecord::Schema.define(version: 2021_09_06_165100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2021_09_06_164649) do
     t.bigint "venues_id", null: false
     t.index ["organization_id"], name: "index_events_on_organization_id"
     t.index ["venues_id"], name: "index_events_on_venues_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "organizations", force: :cascade do |t|
