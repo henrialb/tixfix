b30d606f9af2fcde479771ad8159731e7d23e162
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-06 19:55:18 +0200

    added validations to venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,9 @@
 class Venue < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  has_many :events
+
+  validates_presence_of :name, :address
+  validates :address, uniqueness: { scope: :name}
 end
