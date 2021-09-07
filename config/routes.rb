c29e2e032d3db51430b92c490e19735322e7b5b9
Henrique Albuquerque <henrique.alb@me.com>
2021-09-07 12:30:35 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :organizations, only: [:new, :create, :edit, :update, :show]
-
   resources :events, only: [:index, :show, :new, :create, :edit, :update]
-
   resource :event_categories, only: [:new, :create, :edit, :update]
-
   resources :orders, only: [:index, :show, :new, :create]
-
   resources :tickets, only: [:index, :show, :new, :create]
-
   resources :venues, only: [:index, :show]
-
   resources :clients, only: [:index, :show, :new, :create, :edit, :update]
 
   root to: 'pages#home'
