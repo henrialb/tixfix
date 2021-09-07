fcbc4e818bfeb409f8369231df22788d1f96d467
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-07 13:21:55 +0200

    route setup -- avoid nesting at all cost

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :organizations, only: [:new, :create, :edit, :update, :show]
+
+  resources :events, only: [:index, :show, :new, :create, :edit, :update]
+
+  resource :event_categories, only: [:new, :create, :edit, :update]
+
+  resources :orders, only: [:index, :show, :new, :create]
+
+  resources :tickets, only: [:index, :show, :new, :create]
+
+  resources :venues, only: [:index, :show]
+
+  resources :clients, only: [:index, :show, :new, :create, :edit, :update]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
