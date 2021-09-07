21aa9d9afe61ff0bf6f5bd72a9d3e28d2ad9b38d
Henrique Albuquerque <henrique.alb@me.com>
2021-09-07 12:43:03 +0100

    rewrote devise with except

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :organizations, only: [:new, :create, :edit, :update, :show]
-  resources :events, only: [:index, :show, :new, :create, :edit, :update]
-  resource :event_categories, only: [:new, :create, :edit, :update]
-  resources :orders, only: [:index, :show, :new, :create]
-  resources :tickets, only: [:index, :show, :new, :create]
+  resources :organizations, except: [:index, :destroy]
+  resources :events, except: :destroy
+  resource :event_categories, except: [:index, :show, :destroy]
+  resources :orders, except: [:edit, :update, :destroy]
+  resources :tickets, except: [:edit, :update, :destroy]
   resources :venues, only: [:index, :show]
-  resources :clients, only: [:index, :show, :new, :create, :edit, :update]
+  resources :clients, except: :destroy
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
