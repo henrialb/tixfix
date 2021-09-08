3407720177fa51e3bd93a69c7fdfe1ab064c061a
Pedro Rato <rato.pedromoreira@gmail.com>
2021-09-08 18:32:02 +0100

    Added #Show and #Edit View

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
-  resources :organizations, except: [:index, :destroy]
+  resources :organizations, except: [:destroy]
   resources :events, except: :destroy
   resource :event_categories, except: [:index, :show, :destroy]
   resources :orders, except: [:edit, :update, :destroy]
