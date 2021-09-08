395a4ba7a0d2d6d7aa11c283087bd30a53716d94
Henrique Albuquerque <henrique.alb@me.com>
2021-09-08 16:48:22 +0100

    remove null false from organization_id in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_171818) do
+ActiveRecord::Schema.define(version: 2021_09_08_154450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_171818) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "role", default: 0, null: false
-    t.bigint "organization_id", null: false
+    t.bigint "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
