5c0d2c1068cc737dcfb804e5739974bb6af12dcd
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-10 20:44:25 +0200

    order creation flow

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :set_event, only: [:new, :create, :index, :show]
+
   def index
     @orders = policy_scope(Order)
   end
@@ -8,18 +10,47 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @event = Event.find(params[:event_id])
     @order = Order.new
     authorize @order
   end
 
   def create
+
+    @order = Order.create!(event: @event)
+
+    authorize @order
+
+    @event.event_categories.each do |category|
+
+      if params[:tickets_for].first.keys.include?(category.id.to_s)
+
+        n_tickets = params[:tickets_for].first[category.id.to_s].to_i
+
+        n_tickets.times do
+          Ticket.create!(
+            event_category: category,
+            order: @order,
+            qr_code: "TODO",
+            is_used: false,
+            )
+        end
+      end
+    end
+
+    if @order.save!
+      redirect_to event_orders_path(@event.id), notice: 'Order successfully created.'
+    else
+      flash.now[:alert] = "Error: #{@order.errors.full_messages.join("\n")}"
+      render :new
+    end
+
+
   end
 
   private
 
   def orders_params
-    params.require(:order).permit(:event_id, :client_id)
+    params.require(:order).permit(:event_id, :client_id, :tickets_for)
   end
 
   def set_order
@@ -27,4 +58,7 @@ class OrdersController < ApplicationController
     authorize @order
   end
 
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 end
