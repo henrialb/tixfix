595be45315eac7a16ba6c43313d8251f7bd972d2
Pedro Rato <rato.pedromoreira@gmail.com>
2021-09-13 18:07:22 +0100

    Added hex generator and qr code logic, fitted QR code to Order#show page

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,9 +6,20 @@ class Ticket < ApplicationRecord
 
   before_validation :generate_hex
 
+  def qr_code
+    qr = RQRCode::QRCode.new(self.hex)
+    svg = qr.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      standalone: true,
+      module_size: 3
+    ).html_safe
+  end
+
   private
 
   def generate_hex
-    self.qr_code = SecureRandom.hex
+    self.hex = SecureRandom.hex
   end
 end
