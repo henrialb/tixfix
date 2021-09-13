f1e1cfef1ba7d01185b613d77a1f5cb26434823d
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-13 12:41:01 +0200

    fixed navbar to top

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,6 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
-  if (navbar) {
+  if ((navbar) && (location.pathname === "/")) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-white');
