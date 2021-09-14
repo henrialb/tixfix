85e29e54325929d0fb42ec05f99d0aff024b2c6d
Henrique Albuquerque <henrique.alb@me.com>
2021-09-14 17:07:41 +0100

    Created view for validation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -12,19 +12,24 @@ class TicketsController < ApplicationController
   end
 
   def validation
-    ticket = Ticket.find_by(hex: params[:hash])
+    @ticket = Ticket.find_by(hex: params[:hash])
+    authorize @ticket
 
-    if current_organization == ticket.order.event.organization
+    if current_organization == @ticket.order.event.organization
       # Check if user has the right authorization and has validation mode ON (create this?)
       validation_mode = current_user.is_validation
 
       # Check if event is live
-      live_starts_at = ticket.order.event.starts_at - 60 * 60 * 2 # validation starts 2 hours before event start
-      live_ends_at = ticket.order.event.ends_at + 60 * 60 # ends 1 hour after event end
+      live_starts_at = @ticket.order.event.starts_at - 60 * 60 * 2 # validation starts 2 hours before event start
+      live_ends_at = @ticket.order.event.ends_at + 60 * 60 # ends 1 hour after event end
       live = true if Time.now > live_starts_at && Time.now < live_ends_at
 
-      # Validate ticket
-      ticket.update(is_used: true) if validation_mode && live
+      # Validate @ticket
+      @ticket.is_used = true if validation_mode && live
+
+      unless @ticket.save
+        redirect_to user_registration_path, notice: "You are not authorized to validate!"
+      end
     end
   end
 end
--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -3,9 +3,13 @@ class TicketPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
 
-    def index
-      admin_or_manager
-    end
+  def index
+    admin_or_manager
+  end
+
+  def validation?
+    admin_or_manager
   end
 end
