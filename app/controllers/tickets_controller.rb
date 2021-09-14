9a06efb22707c765d11360b4ee345e499fca7763
Henrique Albuquerque <henrique.alb@me.com>
2021-09-14 15:41:59 +0100

    validation working

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,31 +13,34 @@ class TicketsController < ApplicationController
 
   def validation
     ticket = Ticket.find_by(hex: params[:hash])
+    raise # Testing only - remove!
 
     if current_organization == ticket.order.event.organization
       # Check if user has the right authorization and has validation mode ON (create this?)
       validation_mode = current_user.is_validation
+
       # Check if event is live
       live_starts_at = ticket.order.event.starts_at - 60 * 60 * 2 # validation starts 2 hours before event start
       live_ends_at = ticket.order.event.ends_at + 60 * 60 # ends 1 hour after event end
-      Time.now > live_starts_at && Time.now < live_ends_at ? live = true : live = false
-      raise # Testing only - remove!
+      live = true if Time.now > live_starts_at && Time.now < live_ends_at
 
       # Validate ticket
-      #ticket.is_used = true
-      #ticket.save! # necessary?
+      if validation_mode && live
+        ticket.update(is_used: true)
+      end
 
     # Validation not authorized
+    # If user belongs to organization and has the authorization level to validate tixs,
     elsif current_user.is_validation == false && current_organization = ticket.order.event.organization
+      # Ask if they want to turn validation mode ON
       print "Would you like to turn Validation mode on?"
       input = gets.strip
-
-
-
-      # If user belongs to organization and has the authorization level to validate tixs,
-      # Ask if they want to turn validation mode ON
-
-
+      if input == "yes"
+        current_user.is_validation = true
+        print "Validation mode enabled"
+      else
+        print "Validation mode disabled"
+      end
     end
   end
 end
