bb821db2058c517f1e60f48bcb7702bfe590cddf
Henrique Albuquerque <henrique.alb@me.com>
2021-09-14 17:49:19 +0100

    Finished logic for ticket validation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -23,12 +23,21 @@ class TicketsController < ApplicationController
       live_starts_at = @ticket.order.event.starts_at - 60 * 60 * 2 # validation starts 2 hours before event start
       live_ends_at = @ticket.order.event.ends_at + 60 * 60 # ends 1 hour after event end
       live = true if Time.now > live_starts_at && Time.now < live_ends_at
+      # live = true if Time.now < live_starts_at && Time.now < live_ends_at # FOR TESTING
 
       # Validate @ticket
-      @ticket.is_used = true if validation_mode && live
-
-      unless @ticket.save
-        redirect_to user_registration_path, notice: "You are not authorized to validate!"
+      if validation_mode && live
+        if @ticket.is_used
+          @success = false
+        else
+          @ticket.update(is_used: true)
+          @success = true
+        end
+        # redirect_to edit_user_registration_path, notice: "You are not authorized to validate!"
+      elsif !validation_mode
+        redirect_to event_order_path(@ticket.order.event, @ticket.order), notice: "Validation mode is off!"
+      else
+        redirect_to event_order_path(@ticket.order.event, @ticket.order), notice: "This event is not live yet!"
       end
     end
   end
