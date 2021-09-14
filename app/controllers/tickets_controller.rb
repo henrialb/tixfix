7c7a70e980abf9e14b5850f6b101de64448427f7
Henrique Albuquerque <henrique.alb@me.com>
2021-09-14 12:13:53 +0100

    created validation route and controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,4 +10,17 @@ class TicketsController < ApplicationController
 
   def create
   end
+
+  def validation
+    ticket = Ticket.find_by(hex: params[:hash])
+    raise
+    if current_organization == ticket.order.event.organization
+      # Check if user has the right authorization and has validation mode ON (create this?)
+      # Check if event is live
+      # Validate ticket
+    else
+      # Validation not authorized
+      # If user belongs to organization and has the authorization level to validate tixs, ask if they want to turn validation mode ON
+    end
+  end
 end
