a2cc9ec6f3facf3ed3c04280e914c2f7126276ed
Henrique Albuquerque <henrique.alb@me.com>
2021-09-14 14:59:54 +0100

    Started logic for ticket validation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,14 +13,31 @@ class TicketsController < ApplicationController
 
   def validation
     ticket = Ticket.find_by(hex: params[:hash])
-    raise
+
     if current_organization == ticket.order.event.organization
       # Check if user has the right authorization and has validation mode ON (create this?)
+      validation_mode = current_user.is_validation
       # Check if event is live
+      live_starts_at = ticket.order.event.starts_at - 60 * 60 * 2 # validation starts 2 hours before event start
+      live_ends_at = ticket.order.event.ends_at + 60 * 60 # ends 1 hour after event end
+      Time.now > live_starts_at && Time.now < live_ends_at ? live = true : live = false
+      raise # Testing only - remove!
+
       # Validate ticket
-    else
-      # Validation not authorized
-      # If user belongs to organization and has the authorization level to validate tixs, ask if they want to turn validation mode ON
+      #ticket.is_used = true
+      #ticket.save! # necessary?
+
+    # Validation not authorized
+    elsif current_user.is_validation == false && current_organization = ticket.order.event.organization
+      print "Would you like to turn Validation mode on?"
+      input = gets.strip
+
+
+
+      # If user belongs to organization and has the authorization
+      # level to validate tixs, ask if they want to turn validation mode ON
+
+
     end
   end
 end
