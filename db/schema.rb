5e23187f922262eca674c8e24345beb9eafe3dc7
Henrique Albuquerque <henrique.alb@me.com>
2021-09-15 12:50:41 +0100

    Fixed navbar validation mode visibility

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_09_14_140524) do
+ActiveRecord::Schema.define(version: 2021_09_15_104026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
